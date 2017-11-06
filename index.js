585509f62defa23735f7963683c97895ac17927a
Eugene Kurlovich <zhenikpggkurlovich@gmail.com>
2017-11-06 16:04:51 +0300

    modified server

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const http = require('http');
-const fs = require('fs');
+
 const articlesControllers = require('./controllers/articles');
 const commentsController = require('./controllers/comments');
 const getController = require('./controllers/get');
 
-
-let LogPath = "./Logs/log.txt";
-
 const hostname = '127.0.0.1';
 const port = 3000;
-let File;
 
 const handlers = {
     '/api/articles/readAll': articlesControllers.readAll,
@@ -19,7 +15,7 @@ const handlers = {
     '/api/articles/delete': articlesControllers.deleteArt,
     '/api/comments/create': commentsController.create,
     '/api/comments/deleteCom': commentsController.deleteCom,
-    '/api/logs':getJSONlogs,
+
     '/' : getController.indexx,
     '/index.html' :getController.indexx,
     '/app.js' : getController.app,
@@ -27,17 +23,6 @@ const handlers = {
     '/form.js' : getController.formjs,
     '/site.css' : getController.css
 };
-
-function Log(data) {
-    fs.write(File, data, function (err) {
-        if (err) {
-        }
-    });
-}
-
-fs.open(LogPath, 'w', function (err, fd) {
-    File = fd;
-});
 
 const server = http.createServer((req, res) => {
     parseBodyJson(req, (err, payload) => {
@@ -64,12 +49,6 @@ function getHandler(url) {
     return handlers[url] || notFound;
 }
 
-function sum(req, res, payload, cb) {
-    const result = { c: payload.a + payload.b };
-
-    cb(null, result);
-}
-
 function notFound(req, res, payload, cb) {
     cb({ code: 404, message: 'Not found' });
 }
@@ -80,8 +59,6 @@ function parseBodyJson(req, cb) {
         body.push(chunk);
     }).on('end', function () {
         body = Buffer.concat(body).toString();
-        Log(body);
-        console.log("body : " + body);
         if (body !== "") {
             params = JSON.parse(body);
             cb(null, params);
@@ -90,8 +67,4 @@ function parseBodyJson(req, cb) {
             cb(null, null);
         }
     });
-}
-
-function getJSONlogs(req, res, payload, cb) {
-    return fs.readFile(LogPath, function (err, data) { cb(null, data.toString()) });
 }
